a5ea9011340c51e9a7bba57fb28eb210c1cef20e
ashfaqjuniv2005 <ashfaqhossain830@gmail.com>
2025-05-29 12:20:28 +0600

    Create A+B again.cpp

---
+++ A+B again.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int num;
+        cin>>num;
+        cout<<(num/10)+(num%10)<<endl;
+    }
+    return 0;
+}
