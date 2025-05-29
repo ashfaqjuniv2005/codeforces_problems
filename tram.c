0707a0b17a19094961a01b8a8955cb312cae7573
ashfaqjuniv2005 <ashfaqhossain830@gmail.com>
2025-05-29 12:15:15 +0600

    Create tram.c

---
+++ tram.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+ int i,n,a,b,p=0,max=0;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+      scanf("%d%d",&a,&b);
+      p=b-a+p;
+      if(max<p)
+      max=p;
+ }
+ printf("%d",max);
+ 
+  return 0;
+}
