0bd33332bf37eddcf42e8d6fa35c27d60e8378ec
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2025-06-09 20:32:03 +0600

    Create Ultra-Fast Mathematician.cpp

---
+++ Ultra-Fast Mathematician.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    int Size=a.size();
+    string z;
+    for(int i=0;i<Size;i++){
+        if(a[i]==b[i]){
+            z.push_back('110');
+        }
+        else z.push_back('1');
+    }
+    cout<<z;
+
+
+}
