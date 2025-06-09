fde02fe424d8d00b3f646a71f0c89b95d7e053ed
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2025-06-09 20:43:19 +0600

    Create Petya and Strings.c

---
+++ Petya and Strings.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+void capital(char s[100])
+{
+    for(int i=0;s[i]!='\0';i++)
+    {
+        if(s[i]>=97)
+        s[i]=s[i]-32;
+    }
+}
+int main()
+{
+    char s[100],a[100];
+    gets(s);
+    gets(a);
+    capital(s);
+    capital(a);
+    int i=strcmp(s,a);
+    printf("%d",i);
+    return 0;
+}
