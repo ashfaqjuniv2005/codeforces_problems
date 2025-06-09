5638e532627f3094ed3893b0a1bfe07bc6171a0f
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2025-06-09 20:44:12 +0600

    Create  Minimize!.cpp

---
+++ Minimize!.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int c;
+        int value = (c - a) + (b - c);
+        cout << value << endl;
+    }
+    return 0;
+}
