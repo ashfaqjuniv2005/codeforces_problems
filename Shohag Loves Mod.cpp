f56fe23c44a3f68c93ecb1daa53dc1e4237b2d70
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2025-06-11 11:20:55 +0600

    Create Shohag Loves Mod.cpp

---
+++ Shohag Loves Mod.cpp
@@ -0,0 +1,17 @@
+//collected from tourist
+
+#include<bits/stdc++.h>
+using namespace std;
+void slove() {
+	int n;cin>>n;
+	for(int i=1;i<=n;i++) {
+		cout<<2*i-1<<" ";
+	}
+	cout<<"\n";
+}
+int main(){
+	int T;
+	cin >> T;
+	while (T--) slove();
+	return 0;
+}
