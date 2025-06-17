1ab5543aea7779f97f0aaa0e9c3dc17d4b311669
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2025-06-17 18:11:57 +0600

    Create trippi tropy.cpp

---
+++ trippi tropy.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+          ios_base::sync_with_stdio(false);
+          cin.tie(0);
+          cout.tie(0);
+          
+          int n;
+          cin>>n;
+          for(int i=0;i<n;i++){
+                    string a,b,c;
+                    cin>>a>>b>>c;
+                    cout<<a[0]<<b[0]<<c[0];
+                    cout<<endl;
+          }
+          return 0;
+}
