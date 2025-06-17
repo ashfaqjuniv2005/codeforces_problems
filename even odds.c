02c984f90406fb03e39f8eeafaf70158931792c6
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2025-06-17 18:24:49 +0600

    Create even odds.c

---
+++ even odds.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    long long int n,k,a,b;
+    scanf("%lld %lld",&n,&k);
+    a=k*2-1;
+    if(a>n)
+    {
+        if(n%2==0)
+        b=n/2;
+        else
+        b=n/2+1;
+        k=k-b;
+        b=k*2;
+        printf("%lld",b);
+    }
+    else
+    printf("%lld",a);
+    return 0;
+}
