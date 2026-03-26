7a0005f87768a288c0164a7d2e135341d725d115
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2026-03-26 09:26:14 +0600

    Add New year string.cpp file

---
+++ New year string.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/contest/2182/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;cin>>t;
+while(t--){
+    int n;cin>>n;
+    string s;cin>>s;
+if(s.find("2026")!=-1 || s.find("2025")==-1 ){cout<<0<<endl;}
+else {cout<<1<<endl;}
+}
+}
