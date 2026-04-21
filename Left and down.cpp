ce1cfb48b87dbc04a3626fb400db20ea946ac432
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2026-04-21 12:09:08 +0600

    Create Left and down.cpp

---
+++ Left and down.cpp
@@ -0,0 +1,62 @@
+// Problem Link= https://codeforces.com/contest/2125/problem/B
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define Input  freopen("input.txt", "r", stdin);
+#define Output freopen("output.txt", "w", stdout);
+
+typedef vector<pair<int, int>> vpi;
+typedef vector<vpi> vvpi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+#define pb push_back
+#define all(x) x.begin(), x.end()
+
+#define rip1(i,a,b) for(int i=a;i<b;i++)
+#define rip2(i,a,b) for(int i=a;i<=b;i++)
+#define rip3(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define vi vector<int>
+#define vll vector<long long>
+#define tt int t;cin>>t;while(t--)
+#define fio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ppb pop_back
+#define mii map<int,int>
+#define mll map<long long,long long>
+#define empb emplace_back
+#define vvi vector<vector<int>>
+#define vvl vector<vector<long long>>
+#define Yes cout << "Yes" << endl; 
+#define No cout <<"No"<< endl;
+#define yes cout <<"YES"<< endl;
+#define no cout << "NO" << endl;
+#define pb push_back 
+#define first_max LLONG_MAX
+#define first_min LLONG_MIN
+#define F first
+#define S second
+#define len(x) int((x).size())
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define sersort(v) sort(v.begin(),v.end());
+#define rsort(v) sort(v.rbegin(),v.rend());
+#define flip(v) reverse(v.begin(),v.end());
+
+
+
+
+int main () {
+fio
+tt{
+		ll a,b,k;cin>>a>>b>>k;
+		ll p=__gcd(a,b);
+		cout<<(a/p<=k&&b/p<=k?1:2)<<'\n';
+	}
+    return 0;
+}
